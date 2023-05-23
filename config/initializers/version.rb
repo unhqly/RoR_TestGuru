ed9fe1006f404cc513b6ae5c6dafde3c4d6fa001
Unholy <ya.unholy1@mail.ru>
2023-05-23 13:03:55 +0300

    fixed version.rb

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,14 +1,1 @@
-module Eth
-
-  #Defines the major version of the {Eth} module
-  MAJOR = 1.freeze
-
-  #Defines the minor version of the {Eth} module
-  MINOR = 0.freeze
-
-  #Defines the patch version of the {Eth} module
-  PATCH = 0.freeze
-
-  #Defines the version string of the {Eth} module
-  VERSION = [MAJOR, MINOR, PATCH].join(".").freeze
-end
+TestGuru::Application::VERSIONS = "1.0.0"
