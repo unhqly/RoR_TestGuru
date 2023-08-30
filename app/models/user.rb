bbcad62aecfbce4d463ffb3cb3f2e2b148285725
Unholy <ya.unholy1@mail.ru>
2023-08-30 14:47:00 +0300

    added instance method for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+
+  def finished_tests(level)
+    Test.joins('JOIN result_tests ON result_tests.test_id = tests.id')
+    .joins('JOIN users ON users.id = result_tests.user_id')
+    .where(level: level, users: { id: id })
+  end
 end
