bf8b1f1d0a4eb50d667b57ed442015f97528e308
Unholy <ya.unholy1@mail.ru>
2023-09-19 22:22:25 +0300

    fix

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,11 +1,11 @@
 class Test < ApplicationRecord
-  def self.tests_titles_desc(title)
-    Test.joins('JOIN categories ON tests.category_id = categories.id')
-    .where(categories: { title: title }).order(title: :DESC).pluck(:title)
-  end
-  
   belongs_to :category
   has_many :questions
   has_many :result_tests
   has_many :users, through: :result_tests
+
+  def self.tests_titles_desc(title)
+    Test.joins('JOIN categories ON tests.category_id = categories.id')
+    .where(categories: { title: title }).order(title: :DESC).pluck(:title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
+  has_many :result_tests
+  has_many :tests, through: :result_tests
+
   def finished_tests(level)
     Test.joins('JOIN result_tests ON result_tests.test_id = tests.id')
     .where(level: level, result_tests: { user_id: self.id })
-  end
-
-  has_many :result_tests
-  has_many :tests, through: :result_tests
+  end  
 end
